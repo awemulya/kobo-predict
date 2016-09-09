bbc46725541896a7c52ba93eea5571deff6c1c65
Dipesh Acharya <xtranophilist@gmail.com>
2016-09-09 11:51:37 +0545

    Update gitignore and local_settings.sample

---
+++ onadata/settings/local_settings.sample.py
@@ -0,0 +1,31 @@
+import os
+
+os.environ["DJANGO_SECRET_KEY"] = "@25)**hc^rjaiagb4#&q*84hr*uscsxwr-cv#0joiwj$))obyk"
+os.environ["KOBOCAT_MONGO_HOST"] = "localhost"
+os.environ["CSRF_COOKIE_DOMAIN"] = "localhost"
+
+from onadata.settings.kc_environ import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'kobocat1',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+INSTALLED_APPS = list(INSTALLED_APPS)
+
+INSTALLED_APPS += ['debug_toolbar']
+
+# DEBUG_TOOLBAR_CONFIG = {
+#     'INTERCEPT_REDIRECTS': True
+# }
+
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@25)**hc^rjaiagb4#&q*84hr*uscsxwr-cv#0joiwj$))obyk')
+SESSION_COOKIE_NAME = 'my_cookie'
+SESSION_COOKIE_DOMAIN = 'localhost'
