09d48576fca2923865455512a80fada2aac5aab7
Dipesh Acharya <xtranophilist@gmail.com>
2016-09-12 14:23:42 +0545

    Update local settings sample

--- onadata/settings/local_settings.sample.py
+++ onadata/settings/local_settings.sample.py
@@ -2,7 +2,6 @@ import os
 
 os.environ["DJANGO_SECRET_KEY"] = "@25)**hc^rjaiagb4#&q*84hr*uscsxwr-cv#0joiwj$))obyk"
 os.environ["KOBOCAT_MONGO_HOST"] = "localhost"
-os.environ["CSRF_COOKIE_DOMAIN"] = "localhost"
 
 from onadata.settings.kc_environ import *
 
@@ -26,6 +25,6 @@ INSTALLED_APPS += ['debug_toolbar']
 # }
 
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '@25)**hc^rjaiagb4#&q*84hr*uscsxwr-cv#0joiwj$))obyk')
-SESSION_COOKIE_NAME = 'my_cookie'
+SECRET_KEY = '@25)**hc^rjaiagb4#&q*84hr*uscsxwr-cv#0joiwj$))obyk'
+SESSION_COOKIE_NAME = 'kobo_cookie'
 SESSION_COOKIE_DOMAIN = 'localhost'
