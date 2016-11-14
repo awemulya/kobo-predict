b8d30752048d4f6c46ee6f3603ea3f7aba2ff017
awemulya <awemulya@gmail.com>
2016-11-14 15:15:10 +0545

    migrations fs xforms

--- onadata/apps/fsforms/migrations/0001_initial.py
+++ onadata/apps/fsforms/migrations/0001_initial.py
@@ -16,32 +16,62 @@ class Migration(migrations.Migration):
             name='FieldSightXF',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('scheduled', models.BooleanField(default=True)),
-                ('start', models.DateField(null=True, blank=True)),
-                ('end', models.DateField(null=True, blank=True)),
-                ('site', models.ManyToManyField(related_name='site_forms', to='fieldsight.Site', blank=True)),
+                ('is_staged', models.BooleanField(default=False)),
+                ('is_scheduled', models.BooleanField(default=False)),
+                ('date_created', models.DateTimeField(auto_now=True)),
+                ('date_modified', models.DateTimeField(auto_now=True)),
             ],
+            options={
+                'ordering': ('date_modified',),
+                'db_table': 'fieldsight_forms_data',
+                'verbose_name': 'XForm',
+                'verbose_name_plural': 'XForms',
+            },
         ),
         migrations.CreateModel(
-            name='Stages',
+            name='Schedule',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=256, verbose_name=b'Schedule Name')),
+                ('date_range_start', models.DateField(auto_now=True)),
+                ('date_range_end', models.DateField(auto_now=True)),
+            ],
+            options={
+                'ordering': ('date_range_start',),
+                'db_table': 'fieldsight_forms_schedule',
+                'verbose_name': 'Form Schedule',
+                'verbose_name_plural': 'Form Schedules',
+            },
+        ),
+        migrations.CreateModel(
+            name='Stage',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=256)),
-                ('order', models.IntegerField()),
+                ('order', models.IntegerField(default=0)),
+                ('stage', models.ForeignKey(related_name='parent', blank=True, to='fsforms.Stage', null=True)),
             ],
-        ),
-        migrations.CreateModel(
-            name='SubStage',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=256)),
-                ('stage', models.ForeignKey(related_name='sub_stage', to='fsforms.Stages')),
-            ],
+            options={
+                'ordering': ('order',),
+                'db_table': 'fieldsight_forms_stage',
+                'verbose_name': 'FieldSight Form Stage',
+                'verbose_name_plural': 'FieldSight Form Stages',
+            },
         ),
         migrations.AddField(
             model_name='fieldsightxf',
-            name='sub_stage',
-            field=models.ForeignKey(related_name='sub_stage', blank=True, to='fsforms.SubStage', null=True),
+            name='schedule',
+            field=models.ForeignKey(blank=True, to='fsforms.Schedule', null=True),
+        ),
+        migrations.AddField(
+            model_name='fieldsightxf',
+            name='site',
+            field=models.ManyToManyField(related_name='site_forms', to='fieldsight.Site'),
+        ),
+        migrations.AddField(
+            model_name='fieldsightxf',
+            name='stage',
+            field=models.ForeignKey(blank=True, to='fsforms.Stage', null=True),
         ),
         migrations.AddField(
             model_name='fieldsightxf',
