653e173765e6155b8c30b4f5bf440937a7b26fae
awemulya <awemulya@gmail.com>
2017-02-03 13:12:36 +0545

    custom geo serializer

---
+++ onadata/apps/fieldsight/serializers/GeoJSONSerializer.py
@@ -0,0 +1,8 @@
+from django.contrib.gis.serializers.geojson import Serializer as GeoJSONSerializer
+
+class Serializer(GeoJSONSerializer):
+    def get_dump_object(self, obj):
+        data = super(Serializer, self).get_dump_object(obj)
+        # Extend to your taste
+        data.update(id=obj.pk)
+        return data
