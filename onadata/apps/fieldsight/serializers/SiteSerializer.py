11573d1163ac66bccf760e57c33edc8d2ad87d2e
awemulya <awemulya@gmail.com>
2017-02-26 10:36:33 +0545

    progress in site

--- onadata/apps/fieldsight/serializers/SiteSerializer.py
+++ onadata/apps/fieldsight/serializers/SiteSerializer.py
@@ -5,9 +5,12 @@ from onadata.apps.fieldsight.models import Site
 
 class SiteSerializer(serializers.ModelSerializer):
     type_label = serializers.ReadOnlyField(source='type.name', read_only=True)
-    # project_label = serializers.ReadOnlyField(source='project.name', read_only=True)
+    prog = serializers.SerializerMethodField('get_progress', read_only=True)
     organization_label = serializers.ReadOnlyField(source='project.organization.name', read_only=True)
 
     class Meta:
         model = Site
-        exclude = ('project','is_active','type')
+        exclude = ('project','is_active','type',)
+
+    def get_progress(self, obj):
+        return obj.progress()
