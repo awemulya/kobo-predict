503f9557c458f7dd65aa40b9a4be1de2ff3de4fe
awemulya <awemulya@gmail.com>
2017-05-04 14:22:00 +0545

    project lists api

--- onadata/apps/fieldsight/serializers/ProjectSerializer.py
+++ onadata/apps/fieldsight/serializers/ProjectSerializer.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.geos import Point
 from rest_framework import serializers
 
 from onadata.apps.fieldsight.models import Project, ProjectType
@@ -11,9 +12,16 @@ class ProjectTypeSerializer(serializers.ModelSerializer):
 
 
 class ProjectSerializer(serializers.ModelSerializer):
-    type_label = serializers.ReadOnlyField(source='type.name', read_only=True)
-    organization_label = serializers.ReadOnlyField(source='organization.name', read_only=True)
+    latitude= serializers.CharField()
+    longitude= serializers.CharField()
 
     class Meta:
         model = Project
         exclude = ()
+        read_only_fields = ('logo', 'location')
+
+    def create(self, validated_data):
+        p = Point(float(validated_data.pop('longitude')), float(validated_data.pop('latitude')),srid=4326)
+        validated_data.update({'location':p})
+        project = Project.objects.create(**validated_data)
+        return project
--- onadata/apps/fieldsight/viewsets/ProjectViewSet.py
+++ onadata/apps/fieldsight/viewsets/ProjectViewSet.py
@@ -1,7 +1,19 @@
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.permissions import BasePermission
 
 from onadata.apps.fieldsight.models import ProjectType, Project
 from onadata.apps.fieldsight.serializers.ProjectSerializer import ProjectTypeSerializer, ProjectSerializer
+
+
+class ProjectsPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.group.name in ["Super Admin", "Organization Admin" ]
+
+    def has_object_permission(self, request, view, obj):
+        if request.group.name == "Organization Admin":
+            return obj.organization == request.organization
 
 
 class ProjectTypeViewSet(viewsets.ModelViewSet):
@@ -12,9 +24,19 @@ class ProjectTypeViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectTypeSerializer
 
 
-class ProjectViewSet(viewsets.ModelViewSet):
+class OrganizationsProjectViewSet(viewsets.ModelViewSet):
     """
-    A simple ViewSet for viewing and editing projects.
+    A simple ViewSet for viewing and editing projects Under Organizations.
     """
+
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    # authentication_classes = (BasicAuthentication,)
+    permission_classes = (ProjectsPermission,)
+    parser_classes = (MultiPartParser, FormParser,)
+
+    def filter_queryset(self, queryset):
+        return queryset.filter(organization__pk=self.kwargs.get('pk', None))
+
+    def get_serializer_context(self):
+        return {'request': self.request}
