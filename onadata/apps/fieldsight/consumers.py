8cf34a9402d7fa56476739bd08edb729d48816a5
awemulya <awemulya@gmail.com>
2017-05-11 12:40:20 +0545

    hello sent from client to server and receive back

--- onadata/apps/fieldsight/consumers.py
+++ onadata/apps/fieldsight/consumers.py
@@ -1,9 +1,6 @@
-from django.http import HttpResponse
-from channels.handler import AsgiHandler
-
-def http_consumer(message):
-    # Make standard HTTP response - access ASGI path attribute directly
-    response = HttpResponse("Hello world! You asked for %s" % message.content['path'])
-    # Encode that response into message format (ASGI)
-    for chunk in AsgiHandler.encode_response(response):
-        message.reply_channel.send(chunk)
+def ws_message(message):
+    # ASGI WebSocket packet-received and send-packet message types
+    # both have a "text" key for their textual data.
+    message.reply_channel.send({
+        "text": message.content['text'],
+    })
--- onadata/apps/fieldsight/routing.py
+++ onadata/apps/fieldsight/routing.py
@@ -1,4 +1,6 @@
 from channels.routing import route
+from onadata.apps.fieldsight.consumers import ws_message
+
 channel_routing = [
-    route("http.request", "onadata.apps.fieldsight.consumers.http_consumer"),
+    route("websocket.receive", ws_message),
 ]
