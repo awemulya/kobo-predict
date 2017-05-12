31e4b0a3a1fb10307e44352669f6665348f65e78
awemulya <awemulya@gmail.com>
2017-05-12 15:48:11 +0545

    simple chat room without auth

--- onadata/apps/fieldsight/consumers.py
+++ onadata/apps/fieldsight/consumers.py
@@ -1,18 +1,25 @@
 from channels import Group
+from channels.sessions import channel_session
 
 # Connected to websocket.connect
-def ws_add(message):
-    # Accept the connection
+@channel_session
+def ws_connect(message):
+    # Accept connection
     message.reply_channel.send({"accept": True})
-    # Add to the chat group
-    Group("chat").add(message.reply_channel)
+    # Work out room name from path (ignore slashes)
+    room = message.content['path'].strip("/")
+    # Save room in session and add us to the group
+    message.channel_session['room'] = room
+    Group("chat-%s" % room).add(message.reply_channel)
 
 # Connected to websocket.receive
+@channel_session
 def ws_message(message):
-    Group("chat").send({
-        "text": "[user] %s" % message.content['text'],
+    Group("chat-%s" % message.channel_session['room']).send({
+        "text": message['text'],
     })
 
 # Connected to websocket.disconnect
+@channel_session
 def ws_disconnect(message):
-    Group("chat").discard(message.reply_channel)
+    Group("chat-%s" % message.channel_session['room']).discard(message.reply_channel)
--- onadata/apps/fieldsight/routing.py
+++ onadata/apps/fieldsight/routing.py
@@ -1,8 +1,9 @@
 from channels.routing import route
-from onadata.apps.fieldsight.consumers import ws_add, ws_message, ws_disconnect
+from onadata.apps.fieldsight.consumers import ws_connect, ws_message, ws_disconnect
+
 
 channel_routing = [
-    route("websocket.connect", ws_add),
+    route("websocket.connect", ws_connect),
     route("websocket.receive", ws_message),
     route("websocket.disconnect", ws_disconnect),
 ]
