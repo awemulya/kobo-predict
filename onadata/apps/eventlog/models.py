4373630a6166d7184389dcb754ba43faf2e0b711
awemulya <awemulya@gmail.com>
2017-05-25 17:10:20 +0545

    generic relations for log

--- onadata/apps/eventlog/models.py
+++ onadata/apps/eventlog/models.py
@@ -2,9 +2,8 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-from onadata.apps.fsforms.models import FieldSightXF, FInstance
-from onadata.apps.users.models import UserProfile
-from onadata.apps.fieldsight.models import Site
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class FieldSightLog(models.Model):
@@ -13,17 +12,19 @@ class FieldSightLog(models.Model):
         (1, 'FORM'),
         (2, 'SUBMISSION'),
         (3, 'Site'),
+        (4, 'Project'),
+        (5, 'Organization'),
+        (6, 'Role'),
     )
     type = models.IntegerField(default=0, choices=ACTION_TYPES)
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
-    profile = models.ForeignKey(UserProfile, related_name="log", null=True)
-    form = models.ForeignKey(FieldSightXF, related_name="log", null=True)
-    instance = models.ForeignKey(FInstance, related_name="log", null=True)
-    site = models.ForeignKey(Site, related_name="log", null=True)
     is_seen = models.BooleanField(default=False)
     source = models.ForeignKey(User, related_name='log', null=True)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
 
     class Meta:
         get_latest_by = "date"
