b5fb1e7030159fb0b9f7d3b15a675696c26e4283
awemulya <awemulya@gmail.com>
2017-05-28 12:41:52 +0545

    clean base

--- onadata/apps/eventlog/context_processors.py
+++ onadata/apps/eventlog/context_processors.py
@@ -3,6 +3,7 @@ from onadata.apps.eventlog.models import FieldSightLog
 
 def events(request):
     return {
-        'notifications': FieldSightLog.objects.all()[:10]
+        'notifications': FieldSightLog.objects.all()[:10],
+        'fieldsight_message': []
 
     }
