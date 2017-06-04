b35f2c7f8af9374d1005221945206b62c5842d22
awemulya <awemulya@gmail.com>
2017-06-04 07:58:36 +0545

    user receives chat

--- onadata/apps/fieldsight/consumers.py
+++ onadata/apps/fieldsight/consumers.py
@@ -2,6 +2,47 @@ from channels import Group
 from channels.generic.websockets import WebsocketConsumer, WebsocketDemultiplexer
 
 
+class GroupConsumer(WebsocketConsumer):
+
+    # Set to True to automatically port users from HTTP cookies
+    # (you don't need channel_session_user, this implies it)
+
+    # Set to True if you want it, else leave it out
+    strict_ordering = False
+
+    def connection_groups(self, **kwargs):
+        """
+        Called to return the list of groups to automatically add/remove
+        this connection to/from.
+        """
+        pk = kwargs.get('pk')
+        return ["chat-"+pk]
+
+    def connect(self, message, **kwargs):
+        """
+        Perform things on connection start
+        """
+        # Accept the connection; this is done by default if you don't override
+        # the connect function.
+        pk = kwargs.get('pk')
+        self.message.reply_channel.send({"accept": True})
+
+    def receive(self, text=None, bytes=None, **kwargs):
+        """
+        Called when a message is received with either text or bytes
+        filled out.
+        """
+        # Simple echo
+        pk = kwargs.get('pk')
+        self.send(text=text, bytes=bytes)
+
+    def disconnect(self, message, **kwargs):
+        """
+        Perform things on connection close
+        """
+        pass
+
+
 class OneToOneConsumer(WebsocketConsumer):
 
     # Set to True to automatically port users from HTTP cookies
@@ -15,7 +56,8 @@ class OneToOneConsumer(WebsocketConsumer):
         Called to return the list of groups to automatically add/remove
         this connection to/from.
         """
-        return ["test"]
+        pk = kwargs.get('pk')
+        return ["chat-"+pk]
 
     def connect(self, message, **kwargs):
         """
@@ -23,6 +65,9 @@ class OneToOneConsumer(WebsocketConsumer):
         """
         # Accept the connection; this is done by default if you don't override
         # the connect function.
+        # make this user online
+        pk = kwargs.get('pk')
+        Group("chat-"+pk).add(message.reply_channel)
         self.message.reply_channel.send({"accept": True})
 
     def receive(self, text=None, bytes=None, **kwargs):
@@ -31,52 +76,21 @@ class OneToOneConsumer(WebsocketConsumer):
         filled out.
         """
         # Simple echo
+        pk = kwargs.get('pk')
+        print text
+        receiver_pk = pk
+        # get receiver-id send it to group chat-receiverid
+        Group("chat-"+receiver_pk).send({
+        "text": text})
         self.send(text=text, bytes=bytes)
 
     def disconnect(self, message, **kwargs):
         """
         Perform things on connection close
         """
-        pass
-
-
-class GroupConsumer(WebsocketConsumer):
-
-    # Set to True to automatically port users from HTTP cookies
-    # (you don't need channel_session_user, this implies it)
-    http_user = True
-
-    # Set to True if you want it, else leave it out
-    strict_ordering = False
-
-    def connection_groups(self, **kwargs):
-        """
-        Called to return the list of groups to automatically add/remove
-        this connection to/from.
-        """
-        return ["test"]
-
-    def connect(self, message, **kwargs):
-        """
-        Perform things on connection start
-        """
-        # Accept the connection; this is done by default if you don't override
-        # the connect function.
-        self.message.reply_channel.send({"accept": True})
-
-    def receive(self, text=None, bytes=None, **kwargs):
-        """
-        Called when a message is received with either text or bytes
-        filled out.
-        """
-        # Simple echo
-        self.send(text=text, bytes=bytes)
-
-    def disconnect(self, message, **kwargs):
-        """
-        Perform things on connection close
-        """
-        pass
+        # make this user offline
+        pk = kwargs.get('pk')
+        Group("chat-"+pk).discard(message.reply_channel)
 
 
 class NotificationConsumer(WebsocketConsumer):
