9245bc0eb533b7be4928cf8e7e0518746c91539c
awemulya <awemulya@gmail.com>
2017-06-05 16:40:45 +0545

    asgi

--- onadata/apps/main/asgi.py
+++ onadata/apps/main/asgi.py
@@ -1,6 +1,6 @@
 import os
 from channels.asgi import get_channel_layer
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onadata.apps.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "onadata.apps.settings.common")
 
 channel_layer = get_channel_layer()
