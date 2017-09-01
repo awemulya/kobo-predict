fb4f6ce7b45ce4bf09faed103a7434b505fd9d05
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-09-01 11:34:39 +0545

    notif types

--- onadata/apps/eventlog/models.py
+++ onadata/apps/eventlog/models.py
@@ -25,6 +25,30 @@ class FieldSightLog(models.Model):
         (8, 'SUBMISSION_STATUS'),
         (9, 'UserInvite'),
     )
+
+    NOTIFICATION_TYPES=(
+        (0, ''),
+        (1, ''),
+        (2, ''),
+        (3, ''),
+        (4, ''),
+        (5, ''),
+        (6, ''),
+        (7, ''),
+        (8, ''),
+        (9, ''),
+        (10, ''),
+        (11, ''),
+        (12, ''),
+        (13, ''),
+        (14, ''),
+        (15, ''),
+        (16, ''),
+        (17, ''),
+        (18, ''),
+        (19, ''),
+        )
+    
     type = models.IntegerField(default=0, choices=ACTION_TYPES)
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True, null=True)
