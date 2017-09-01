bb9f34ccfd8382b623dce8e5e89492c38269b758
awemulya <awemulya@gmail.com>
2017-09-01 10:56:59 +0545

    multiple channels for notification

--- onadata/apps/fieldsight/consumers.py
+++ onadata/apps/fieldsight/consumers.py
@@ -138,6 +138,96 @@ class NotificationConsumer(WebsocketConsumer):
         Group("notify-{}".format(pk)).discard(message.reply_channel)
 
 
+class ProjectLevelConsumer(WebsocketConsumer):
+
+    # Set to True to automatically port users from HTTP cookies
+    # (you don't need channel_session_user, this implies it)
+
+    # Set to True if you want it, else leave it out
+    strict_ordering = False
+
+    def connection_groups(self, **kwargs):
+        """
+        Called to return the list of groups to automatically add/remove
+        this connection to/from.
+        """
+        pk = kwargs.get('pk')
+        return ["notify-project-"+pk]
+
+    def connect(self, message, **kwargs):
+        """
+        Perform things on connection start
+        """
+        # Accept the connection; this is done by default if you don't override
+        # the connect function.
+        pk = kwargs.get('pk')
+        Group("notify-project-{}".format(pk)).add(message.reply_channel)
+        self.message.reply_channel.send({"accept": True})
+
+    def receive(self, text=None, bytes=None, **kwargs):
+        """
+        Called when a message is received with either text or bytes
+        filled out.
+        """
+        # Simple echo
+        pk = kwargs.get('pk')
+        print text
+        print bytes
+        self.send(text=text, bytes=bytes)
+
+    def disconnect(self, message, **kwargs):
+        """
+        Perform things on connection close
+        """
+        pk = kwargs.get('pk')
+        Group("notify-project-{}".format(pk)).discard(message.reply_channel)
+
+
+class SiteLevelConsumer(WebsocketConsumer):
+
+    # Set to True to automatically port users from HTTP cookies
+    # (you don't need channel_session_user, this implies it)
+
+    # Set to True if you want it, else leave it out
+    strict_ordering = False
+
+    def connection_groups(self, **kwargs):
+        """
+        Called to return the list of groups to automatically add/remove
+        this connection to/from.
+        """
+        pk = kwargs.get('pk')
+        return ["notify-site-"+pk]
+
+    def connect(self, message, **kwargs):
+        """
+        Perform things on connection start
+        """
+        # Accept the connection; this is done by default if you don't override
+        # the connect function.
+        pk = kwargs.get('pk')
+        Group("notify-site-{}".format(pk)).add(message.reply_channel)
+        self.message.reply_channel.send({"accept": True})
+
+    def receive(self, text=None, bytes=None, **kwargs):
+        """
+        Called when a message is received with either text or bytes
+        filled out.
+        """
+        # Simple echo
+        pk = kwargs.get('pk')
+        print text
+        print bytes
+        self.send(text=text, bytes=bytes)
+
+    def disconnect(self, message, **kwargs):
+        """
+        Perform things on connection close
+        """
+        pk = kwargs.get('pk')
+        Group("notify-site-{}".format(pk)).discard(message.reply_channel)
+
+
 # class Demultiplexer(WebsocketDemultiplexer):
 #
 #     # Wire your JSON consumers here: {stream_name : consumer}
