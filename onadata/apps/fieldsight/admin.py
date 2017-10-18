4e8e55b8ea9625144fdb15a678c7a185d7e99e4c
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-10-18 13:12:36 +0545

    user site list fixed

--- onadata/apps/fieldsight/admin.py
+++ onadata/apps/fieldsight/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserInvite, Organization
+from .models import UserInvite, Organization, Project
 
 admin.site.register(UserInvite)
 admin.site.register(Organization)
+admin.site.register(Project)
