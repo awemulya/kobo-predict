b216c7a17518bfb68133d432e3ee1ed4596e4bf4
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-10-26 16:31:24 +0545

    project response fixed.

--- onadata/apps/fsforms/views.py
+++ onadata/apps/fsforms/views.py
@@ -1185,7 +1185,7 @@ def project_html_export(request, fsxf_id):
     xform = fsxf.xf
     id_string = xform.id_string
     # cursor = get_instances_for_project_field_sight_form(fsxf_id)
-    cursor = FInstance.objects.filter(project_fxf=fsxfs) 
+    cursor = FInstance.objects.filter(project_fxf=fsxf) 
     cursor = list(cursor)
     paginator = Paginator(cursor, limit, request=request)
 
