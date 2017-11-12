51ace727f8c753e7ae4be47159cbc8a5afce10fc
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-11-12 13:10:51 +0545

    hyperlinked api test

--- onadata/apps/fieldsight/views.py
+++ onadata/apps/fieldsight/views.py
@@ -1508,29 +1508,29 @@ class SitedataSubmissionView(TemplateView):
 
         return data
 
-from django.core.files.storage import FileSystemStorage
-from django.http import HttpResponse
+# from django.core.files.storage import FileSystemStorage
+# from django.http import HttpResponse
 
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib.units import inch
+# from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+# from reportlab.lib.styles import getSampleStyleSheet
+# from reportlab.lib.units import inch
 
-def write_pdf_view(request):
-    doc = SimpleDocTemplate("/tmp/somefilename.pdf")
-    styles = getSampleStyleSheet()
-    Story = [Spacer(1,2*inch)]
-    style = styles["Normal"]
-    for i in range(100):
-       bogustext = ("This is Paragraph number %s.  " % i) * 20
-       p = Paragraph(bogustext, style)
-       Story.append(p)
-       Story.append(Spacer(1,0.2*inch))
-    doc.build(Story)
+# def write_pdf_view(request):
+#     doc = SimpleDocTemplate("/tmp/somefilename.pdf")
+#     styles = getSampleStyleSheet()
+#     Story = [Spacer(1,2*inch)]
+#     style = styles["Normal"]
+#     for i in range(100):
+#        bogustext = ("This is Paragraph number %s.  " % i) * 20
+#        p = Paragraph(bogustext, style)
+#        Story.append(p)
+#        Story.append(Spacer(1,0.2*inch))
+#     doc.build(Story)
 
-    fs = FileSystemStorage("/tmp")
-    with fs.open("somefilename.pdf") as pdf:
-        response = HttpResponse(pdf, content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
-        return response
+#     fs = FileSystemStorage("/tmp")
+#     with fs.open("somefilename.pdf") as pdf:
+#         response = HttpResponse(pdf, content_type='application/pdf')
+#         response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+#         return response
 
-    return response
+#     return response
