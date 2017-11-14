2f5c88f9f13e034f4b176fc644d71ec6f3956775
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-11-14 19:55:19 +0545

    notification fixes.

--- onadata/apps/eventlog/models.py
+++ onadata/apps/eventlog/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.contrib.auth.models import User
--- onadata/apps/eventlog/views.py
+++ onadata/apps/eventlog/views.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db.models import Q
 from django.utils.timezone import now
 from django.shortcuts import render, redirect
