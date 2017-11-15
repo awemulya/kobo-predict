26b50835335350e5c0445f41d1226d9c1b506d44
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-11-15 15:22:13 +0545

    reports testng

--- onadata/apps/fieldsight/views.py
+++ onadata/apps/fieldsight/views.py
@@ -1553,20 +1553,9 @@ class SitedataSubmissionView(TemplateView):
 
 
 def project_html_export(request, pk):
-    forms = FieldSightXF.objects.filter(site_id=pk)
-    # site_responses_report(forms)
-    # # data = {}
-    # # for fsxf in forms:
-    # #     data['form_detail'] = fsxf
-    # #     xform = fsxf.xf
-    # #     id_string = xform.id_string
-    # #     data['form_responces'] = get_instances_for_project_field_sight_form(fsxf_id)
-    buffer = BytesIO()
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="My Users.pdf"'
- 
-   
- 
+    response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+    buffer = BytesIO()
     report = MyPrint(buffer, 'Letter')
     pdf = report.print_users(forms)
  
