ad9e0e61d4f043a65c29861902fca38390a418d0
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-11-17 16:44:07 +0545

    api response fixed

--- onadata/apps/fsforms/models.py
+++ onadata/apps/fsforms/models.py
@@ -392,7 +392,7 @@ class FInstance(models.Model):
                         answer = ''
                     if 'label' in first_children:
                         question = first_children['label']
-                    row={'question':question_type, question,answer}
+                    row={'question':question_type, 'question':question,'ans':answer}
                     data.append(row)
 
         def parse_individual_questions(parent_object):
