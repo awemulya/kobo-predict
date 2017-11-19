5051938069404f381d9ec97ee41f605d2420bcd7
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-11-19 16:45:54 +0545

    api test for print

--- onadata/apps/fieldsight/generatereport.py
+++ onadata/apps/fieldsight/generatereport.py
@@ -193,6 +193,7 @@ class MyPrint:
               t1 = Table(self.data, colWidths=(60*mm, None))
               t1.setStyle(ts1)
               elements.append(t1)
+              elements.append(Paragraph("Form Name:========", styles['Normal']))
 
 
             # self.parse_individual_questions(json_question['children'])
