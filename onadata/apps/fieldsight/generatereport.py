240ae809874592b095ecf33f2ca5d67d59ac3cde
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-11-20 11:44:50 +0545

    testing pdf.

--- onadata/apps/fieldsight/generatereport.py
+++ onadata/apps/fieldsight/generatereport.py
@@ -177,18 +177,20 @@ class MyPrint:
             self.media_folder = form_user_name
             elements.append(Paragraph("Form Created By:"+form_user_name, styles['Normal']))
             cursor = get_instaces_for_site_individual_form(form.id)
+            styNormal = styleSheet['Normal']
+            styBackground = ParagraphStyle('background', parent=styNormal, backColor=colors.pink)
+            ts1 = TableStyle([
+                ('ALIGN', (0,0), (-1,0), 'RIGHT'),
+                ('BACKGROUND', (0,0), (-1,0), colors.lightgrey),
+                ('VALIGN', (0,0), (-1,-1), 'TOP'),
+                ('GRID', (0,0), (-1,-1), 0.25, colors.black),
+                    ])
             for instance in cursor:
+              t1 = None
               self.main_answer = instance
               question = json.loads(json_question)
               self.parse_individual_questions(question['children'])
-              styNormal = styleSheet['Normal']
-              styBackground = ParagraphStyle('background', parent=styNormal, backColor=colors.pink)
-              ts1 = TableStyle([
-                  ('ALIGN', (0,0), (-1,0), 'RIGHT'),
-                  ('BACKGROUND', (0,0), (-1,0), colors.lightgrey),
-                  ('VALIGN', (0,0), (-1,-1), 'TOP'),
-                  ('GRID', (0,0), (-1,-1), 0.25, colors.black),
-                      ])
+              
 
               t1 = Table(self.data, colWidths=(60*mm, None))
               t1.setStyle(ts1)
