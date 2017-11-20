64745f300ebf38dfc463cdb73e68ccf3f2f34511
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-11-20 17:19:15 +0545

    getting report data form postgresql test

--- onadata/apps/fieldsight/generatereport.py
+++ onadata/apps/fieldsight/generatereport.py
@@ -197,7 +197,7 @@ class MyPrint:
               t1 = Table(self.data, colWidths=(60*mm, None))
               t1.setStyle(ts1)
               elements.append(Spacer(0,10))
-              # elements.append(t1)
+              elements.append(t1)
               elements.append(Paragraph("===============", styles['Normal']))
               elements.append(Spacer(0,10))
         #     else:
