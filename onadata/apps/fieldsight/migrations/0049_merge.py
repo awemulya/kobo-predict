378b05608a3bd9b43aeda80b95cf487dce3f10db
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2017-12-13 12:06:09 +0545

    mighrations merged

---
+++ onadata/apps/fieldsight/migrations/0049_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fieldsight', '0048_auto_20171212_0804'),
+        ('fieldsight', '0048_auto_20171211_1658'),
+    ]
+
+    operations = [
+    ]
