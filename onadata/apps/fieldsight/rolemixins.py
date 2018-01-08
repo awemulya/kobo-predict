70ac810193766962e3ccc3ab353286fa84100114
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-01-08 14:32:57 +0545

    fixes in api for deploy forms

--- onadata/apps/fieldsight/rolemixins.py
+++ onadata/apps/fieldsight/rolemixins.py
@@ -216,7 +216,7 @@ class SPFmixin(LoginRequiredMixin):
 class FormMixin(LoginRequiredMixin):
     def dispatch(self, request, fsxf_id):
         if request.group.name == "Super Admin":
-                return super(FormMixin, self).dispatch(request, *args, **kwargs)
+                return super(FormMixin, self).dispatch(request, fsxf_id)
 
         user_id = request.user.id
         form = get_object_or_404(FieldSightXF, pk=fsxf_id)
@@ -225,7 +225,7 @@ class FormMixin(LoginRequiredMixin):
             site_id = form.site.id
             user_role = request.roles.filter(user_id = user_id, site_id = site_id, group__name="Reviewer")
             if user_role:
-                return super(FormMixin, self).dispatch(request, *args, **kwargs)
+                return super(FormMixin, self).dispatch(request, fsxf_id)
             project_id=Site.objects.get(pk=site_id).project
         
         else:
@@ -233,12 +233,12 @@ class FormMixin(LoginRequiredMixin):
 
         user_role = request.roles.filter(user_id = user_id, project_id = project_id, group__name="Project Manager")
         if user_role:
-            return super(FormMixin, self).dispatch(request, *args, **kwargs)
+            return super(FormMixin, self).dispatch(request, fsxf_id)
 
         organization_id = Project.objects.get(pk=project_id).organization.id
         user_role_asorgadmin = request.roles.filter(user_id = user_id, organization_id = organization_id, group__name="Organization Admin")
         if user_role_asorgadmin:
-            return super(FormMixin, self).dispatch(request, *args, **kwargs)
+            return super(FormMixin, self).dispatch(request, fsxf_id)
 
         raise PermissionDenied()   
 
