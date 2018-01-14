2e843143b925d475d02ad6060a0946a572d1b607
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-01-14 16:54:38 +0545

    testing

--- onadata/apps/fieldsight/views.py
+++ onadata/apps/fieldsight/views.py
@@ -1992,17 +1992,18 @@ class ProjectStageResponsesStatus(ProjectRoleMixin, View):
                         ss_index.update({head_row.index("Sub Stage :"+ss.name): ss.id})
                         substages.append(ss.name)
 
-            return render(request, 'fieldsight/ProjectStageResponsesStatus.html', {'table_head': table_head, "substages":substages})
-
+            
 
             # data.append(head_row)
             total_cols = len(head_row) - 6 # for non stages
             for site in project.sites.filter(is_active=True, is_survey=False):
-                site_row = [site.identifier, site.name, site.address, site.latitude, site.longitude, site.status]
+                site_row = [site.identifier, site.name]
                 site_row.extend([None]*total_cols)
                 for k, v in ss_index.items():
                     if Stage.objects.filter(project_stage_id=v, site=site).count() == 1:
                         site_sub_stage = Stage.objects.get(project_stage_id=v, site=site)
                         site_row[k] = site_sub_stage.form_status
                 data.append(site_row)
+            
+            return render(request, 'fieldsight/ProjectStageResponsesStatus.html', {'table_head': table_head, "substages":substages, "data":data})
             return HttpResponse(table_head)
