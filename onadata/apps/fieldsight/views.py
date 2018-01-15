c2f1de90db7d3eee4555aa7c165d3a97d4bdb976
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-01-15 18:21:45 +0545

    testing

--- onadata/apps/fieldsight/views.py
+++ onadata/apps/fieldsight/views.py
@@ -1995,7 +1995,7 @@ class ProjectStageResponsesStatus(ProjectRoleMixin, View):
             
 
             # data.append(head_row)
-            total_cols = len(head_row) - 2 # for non stages
+            total_cols = len(head_row) # for non stages
             for site in project.sites.filter(is_active=True, is_survey=False):
                 site_row = [site.identifier, site.name]
                 site_row.extend([None]*total_cols)
@@ -2006,5 +2006,5 @@ class ProjectStageResponsesStatus(ProjectRoleMixin, View):
                 a= site_row
                 data.append(site_row)
             
-            # return render(request, 'fieldsight/ProjectStageResponsesStatus.html', {'table_head': table_head, "substages":substages, "data":data})
+            return render(request, 'fieldsight/ProjectStageResponsesStatus.html', {'table_head': table_head, "substages":substages, "data":data})
             # return HttpResponse(table_head)
