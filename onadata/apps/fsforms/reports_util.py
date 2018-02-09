20840e4630ff403d448145756f1ba91e60417bb4
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-02-09 15:00:54 +0545

    api for recent submissions for site finished.

--- onadata/apps/fsforms/reports_util.py
+++ onadata/apps/fsforms/reports_util.py
@@ -11,7 +11,7 @@ def get_images_for_site(site_id):
     return settings.MONGO_DB.instances.find({"fs_site": str(site_id), "_attachments.mimetype" : "image/jpeg"},{"_attachments" : { '$elemMatch': { "mimetype": "image/jpeg"}}, "_attachments.filename":1, "fs_uuid":1}).sort([( "_id", -1 )]).limit(10)
 
 def get_site_responses_coords(site_id):
-    return settings.MONGO_DB.instances.find({"fs_site": str(site_id), "_geolocation":{"$not":{ "$elemMatch": { "$eq": None } }}},{"_geolocation":1, "fs_uuid":1})
+    return settings.MONGO_DB.instances.find({"fs_site": str(site_id), "_geolocation":{"$not":{ "$elemMatch": { "$eq": None } }}},{"_geolocation":1, "fs_uuid":1, "_submitted_by":1})
 
 def get_instaces_for_site_individual_form(fieldsightxf_id):
     query = {"fs_uuid":str(fieldsightxf_id)}
