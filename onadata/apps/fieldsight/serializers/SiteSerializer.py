4dc71d7e6bcda6a99526fa5379829c35c464de3b
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-02-19 12:35:46 +0545

    regional api created

--- onadata/apps/fieldsight/serializers/SiteSerializer.py
+++ onadata/apps/fieldsight/serializers/SiteSerializer.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django.contrib.gis.geos import Point
 from rest_framework import serializers
-from onadata.apps.fieldsight.models import Site, SiteCreateSurveyImages, ProjectType, Project
+from onadata.apps.fieldsight.models import Site, Region, SiteCreateSurveyImages, ProjectType, Project
 
 
 class SiteSerializer(serializers.ModelSerializer):
