c2e5947e2de33662359fe664801bb09dacd0460b
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-03-22 15:21:01 +0545

    form flag implemented

--- onadata/apps/fsforms/viewsets/XformsViewset.py
+++ onadata/apps/fsforms/viewsets/XformsViewset.py
@@ -16,4 +16,4 @@ class XFormViewSet(viewsets.ReadOnlyModelViewSet):
         if self.request.user.user_roles.filter(group__name="Super Admin").exists():
             return self.queryset
         return self.queryset.filter(Q(user=self.request.user) |
-                Q(user__user_profile__organization=self.request.organization))
+                Q(user__user_profile__organization=self.request.organization), deleted_xform=None)
