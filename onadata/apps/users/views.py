90af764b8d048025e7eebab4733d9ef9ea975b29
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-03-26 14:05:40 +0545

    role remove test

--- onadata/apps/users/views.py
+++ onadata/apps/users/views.py
@@ -330,8 +330,9 @@ def my_profile(request, pk=None):
             roles_SA = roles_SA.filter(organization_id__in=org_ids)
             responses = FInstance.objects.filter(Q(submitted_by = user) & (Q(site__project__organization_id__in=org_ids) | Q(project__organization_id__in=org_ids))).order_by('-date')[:10]
         
-        own_manager_roles=request.user.user_roles.filter(group_id=2, ended_at__isnull=True).values('project_id')
-        own_org_admin=request.user.user_roles.filter(group_id=1, ended_at__isnull=True).values('organization_id')
+        own_manager_roles=request.user.user_roles.filter(group_id=2, ended_at__isnull=True).values_list('project_id', flat=True)
+        own_org_admin=request.user.user_roles.filter(group_id=1, ended_at__isnull=True).values_list('organization_id', flat=True)
+
         return render(request, 'users/profile.html', {'obj': profile, 'own_orgs':own_org_admin,'own_projects':own_manager_roles,'roles_org': roles_org, 'roles_project': roles_project, 'roles_site': roles_reviewer, 'roles_SA': roles_SA, 'roles_reviewer': roles_reviewer, 'responses': responses })
 
 class UsersListView(TemplateView, SuperAdminMixin):
