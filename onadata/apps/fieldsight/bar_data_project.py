0d04b5df4af3c42d86bbc0ab73de54982ce310e3
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-03-30 12:06:02 +0545

    notification to superadmin fix

--- onadata/apps/fieldsight/bar_data_project.py
+++ onadata/apps/fieldsight/bar_data_project.py
@@ -4,7 +4,8 @@ from collections import OrderedDict
 class BarGenerator(object):
     def __init__(self, sites):
         self.data = OrderedDict()
-        self.data['0-10'] = 0
+        self.data['0'] = 0
+        self.data['1-10'] = 0
         self.data['10-20'] = 0
         self.data['20-30'] = 0
         self.data['30-40'] = 0
@@ -19,13 +20,14 @@ class BarGenerator(object):
             self.data[progress_range] +=1
 
     def get_range(self, progress):
-        if progress in range(0,10): return self.data.keys()[0]
-        if progress in range(10,20): return self.data.keys()[1]
-        if progress in range(20,30): return self.data.keys()[2]
-        if progress in range(30,40): return self.data.keys()[3]
-        if progress in range(40,50): return self.data.keys()[4]
-        if progress in range(50,60): return self.data.keys()[5]
-        if progress in range(60,70): return self.data.keys()[6]
-        if progress in range(70,80): return self.data.keys()[7]
-        if progress in range(80,90): return self.data.keys()[8]
-        if progress in range(90,101): return self.data.keys()[9]
+        if progress == 0: return self.data.keys()[0]
+        if progress in range(0,10): return self.data.keys()[1]
+        if progress in range(10,20): return self.data.keys()[2]
+        if progress in range(20,30): return self.data.keys()[3]
+        if progress in range(30,40): return self.data.keys()[4]
+        if progress in range(40,50): return self.data.keys()[5]
+        if progress in range(50,60): return self.data.keys()[6]
+        if progress in range(60,70): return self.data.keys()[7]
+        if progress in range(70,80): return self.data.keys()[8]
+        if progress in range(80,90): return self.data.keys()[9]
+        if progress in range(90,101): return self.data.keys()[10]
