38faa823eb20674c92841876da307374dfdeba05
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-04-01 11:24:44 +0545

    staffs models created

---
+++ onadata/apps/eventlog/migrations/0027_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eventlog', '0024_auto_20180309_1406'),
+        ('eventlog', '0026_celerytaskprogress_date_updateded'),
+    ]
+
+    operations = [
+    ]
