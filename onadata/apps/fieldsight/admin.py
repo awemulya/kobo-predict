4e61cd5934b8f195b44c0976dae6cd9eb0aedd4d
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-04-09 11:23:47 +0545

    fix in tasks py

--- onadata/apps/fieldsight/admin.py
+++ onadata/apps/fieldsight/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import UserInvite, Organization, Project, Site, Region
+from .models import UserInvite, Organization, Project, Site, Region, SiteType
 
 admin.site.register(UserInvite)
 admin.site.register(Organization)
 admin.site.register(Project)
 admin.site.register(Site)
 admin.site.register(Region)
+admin.site.register(SiteType)
