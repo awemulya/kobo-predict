943ca3ed97f3102285631c6f202623f7982b698b
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-04-13 11:32:07 +0545

    fixes

--- onadata/apps/staff/serializers/staffSerializer.py
+++ onadata/apps/staff/serializers/staffSerializer.py
@@ -25,22 +25,15 @@ class StaffSerializer(serializers.ModelSerializer):
         exclude = ('created_by', 'team', 'created_date', 'updated_date', 'is_deleted',)
 
     def create(self, validated_data):
-        bank_id = validated_data.pop('bank') if 'bank' in validated_data else 0
+        bank_id = validated_data.pop('bank') if 'bank' in validated_data else None
         instance = Staff.objects.create(**validated_data)
         try:
-            # if bank_id == None:
-            #     bank_id
-
-            if bank_id >= 1:
-                instance.bank_id = bank_id
+            if bank_id:
                 instance.bank_name = ''
            
-            elif bank_id == 0:
-                if instance.bank_name == "":
-                    raise ValidationError("Got empty bank name. Provide either bank id or bank name.")
-                instance.bank = None 
             else:
-                raise ValidationError("Got empty bank name. Provide either bank id or bank name.")    
+                if instance.bank_name == "":
+                    raise ValidationError("Got empty bank name. Provide either bank id or bank name.")     
             instance.save()
         except Exception as e:
             raise ValidationError("Got error on: {}".format(e))
