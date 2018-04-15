84f70e7338a7cbff88a892cab41706f1a7c571ad
awemulya <awemulya@gmail.com>
2018-04-15 15:58:53 +0545

    to many sites loading timeout live bypass

--- onadata/apps/fieldsight/views.py
+++ onadata/apps/fieldsight/views.py
@@ -201,16 +201,30 @@ class Project_dashboard(ProjectRoleMixin, TemplateView):
 
         peoples_involved = obj.project_roles.filter(ended_at__isnull=True).distinct('user')
         # total_sites = obj.sites.filter(is_active=True, is_survey=False).count()
-        sites = obj.sites.filter(is_active=True, is_survey=False).prefetch_related('site_forms', "site_instances")
-        data = serialize('custom_geojson', sites, geometry_field='location',
-                         fields=('location', 'id',))
+
 
         total_sites = obj.sites.filter(is_active=True, is_survey=False).count()
         total_survey_sites = 0
         outstanding, flagged, approved, rejected = obj.get_submissions_count()
-        bar_graph = BarGenerator(sites)
-        line_chart = LineChartGenerator(obj)
-        line_chart_data = line_chart.data()
+        if obj.id == 137:
+            progress_data = []
+            progress_labels = []
+            cumulative_labels = []
+            cumulative_data = []
+            data = []
+            sites = []
+        else:
+            sites = obj.sites.filter(is_active=True, is_survey=False).prefetch_related('site_forms', "site_instances")
+            data = serialize('custom_geojson', sites, geometry_field='location',
+                         fields=('location', 'id',))
+            bar_graph = BarGenerator(sites)
+            progress_data = bar_graph.data.values()
+            progress_labels = bar_graph.data.keys()
+            line_chart = LineChartGenerator(obj)
+            line_chart_data = line_chart.data()
+            cumulative_labels = line_chart_data.keys()
+            cumulative_data = line_chart_data.values()
+
         roles_project = UserRole.objects.filter(organization__isnull=False,
                                                 project_id = self.kwargs.get('pk'),
                                                 site__isnull=True, ended_at__isnull=True)
@@ -226,10 +240,10 @@ class Project_dashboard(ProjectRoleMixin, TemplateView):
             'approved': approved,
             'rejected': rejected,
             'data': data,
-            'cumulative_data': line_chart_data.values(),
-            'cumulative_labels': line_chart_data.keys(),
-            'progress_data': bar_graph.data.values(),
-            'progress_labels': bar_graph.data.keys(),
+            'cumulative_data': cumulative_data,
+            'cumulative_labels': cumulative_labels,
+            'progress_data': progress_data,
+            'progress_labels': progress_labels,
             'roles_project': roles_project,
             'total_submissions': flagged + approved + rejected + outstanding
     }
