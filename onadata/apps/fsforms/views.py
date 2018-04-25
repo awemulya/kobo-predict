9edeb287f0eca235f7020d5da32a0d0440040123
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-04-25 12:34:11 +0545

    testing for link in detail report

--- onadata/apps/fsforms/views.py
+++ onadata/apps/fsforms/views.py
@@ -1356,7 +1356,7 @@ class FullResponseTable(ReadonlyFormMixin, View):
 
         export = context['export']
         sections = list(export.labels.items())
-        question_names = export.section.items()[0][1]
+        question_names = export.sections.items()[0][1]
         section, labels = sections[0]
         id_index = labels.index('_id')
 
