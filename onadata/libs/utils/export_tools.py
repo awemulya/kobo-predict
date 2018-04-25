1482440dd638124ef1d18599be92bf9d5d925a5f
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-04-25 17:23:30 +0545

    testing for excel reports

--- onadata/libs/utils/export_tools.py
+++ onadata/libs/utils/export_tools.py
@@ -580,7 +580,7 @@ class ExportBuilder(object):
             data_new = []
             for f in fields:
                 if args[2] and f in media_attributes:
-                    data_new.append('=HYPERLINK("https://app.fieldsight.org/attachment/medium?media_file='+args[2].user.username+'/attachments/'+data.get(f)+'"; "SO")')
+                    data_new.append('=HYPERLINK("https://app.fieldsight.org/attachment/medium?media_file='+args[2].user.username+'/attachments/'+data.get(f)+'", "link")')
                 else:    
                     data_new.append(data.get(f))
             work_sheet.append(data_new)
