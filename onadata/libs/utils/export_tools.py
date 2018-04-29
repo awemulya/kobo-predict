8f9a93371d320490337568dd914a4344f930f11e
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-04-29 16:19:06 +0545

    fix in nra update

--- onadata/libs/utils/export_tools.py
+++ onadata/libs/utils/export_tools.py
@@ -523,7 +523,9 @@ class ExportBuilder(object):
                 if f in media_attributes:
                     http = urllib3.PoolManager()
                     #data_new.append('=HYPERLINK("http://nrakc.fieldsight.org/attachment/medium?media_file='+xform.user.username+'/attachments/'+data.get(f)+'", "Attachment")')
-                    your_image = openpyxl.drawing.Image('http://nrakc.fieldsight.org/attachment/medium?media_file='+xform.user.username+'/attachments/'+data.get(f))
+                    r = http.request('GET', 'http://nrakc.fieldsight.org/attachment/medium?media_file='+xform.user.username+'/attachments/'+data.get(f))
+                    image_file = io.BytesIO(r.data)
+                    your_image = Image(image_file)
                     your_image.anchor(ws.cell('A16'))
                     img = Image(your_image)
                     work_sheet.add_image(img)
