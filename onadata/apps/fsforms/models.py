e80bafe286ea1f751e4c42aeea33b70882375bf5
awemulya <awemulya@gmail.com>
2018-04-29 14:43:42 +0545

    new site own types substages copied

--- onadata/apps/fsforms/models.py
+++ onadata/apps/fsforms/models.py
@@ -563,11 +563,15 @@ def copy_stages_from_project(sender, **kwargs):
         project_main_stages = project.stages.filter(stage__isnull=True)
         for pms  in project_main_stages:
             project_sub_stages = Stage.objects.filter(stage__id=pms.pk, stage_forms__is_deleted=False)
-            site_main_stage = Stage(name=pms.name, order=pms.order, site=site, description=pms.description, project_stage_id=pms.id)
+            site_main_stage = Stage(name=pms.name, order=pms.order, site=site, description=pms.description,
+                                    project_stage_id=pms.id, weight=pms.weight)
             site_main_stage.save()
             for pss in project_sub_stages:
+                if pss.tags and site.type:
+                    if not site.type.id in pss.tags:
+                        continue
                 site_sub_stage = Stage(name=pss.name, order=pss.order, site=site,
-                               description=pss.description, stage=site_main_stage, project_stage_id=pss.id)
+                               description=pss.description, stage=site_main_stage, project_stage_id=pss.id, weight=pss.weight)
                 site_sub_stage.save()
                 if FieldSightXF.objects.filter(stage=pss).exists():
                     fsxf = pss.stage_forms
