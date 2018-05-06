97b42b8b5143e0ff97569402727a5fde455740eb
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-05-06 12:43:07 +0545

    form recovery command added.

--- onadata/apps/fsforms/management/commands/form_recovery.py
+++ onadata/apps/fsforms/management/commands/form_recovery.py
@@ -1,9 +1,12 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth.models import Group
 from onadata.apps.viewer.models.parsed_instance import dict_for_mongo, _encode_for_mongo, xform_instances
+from django.conf import settings
 
 class Command(BaseCommand):
     help = 'Create default groups'
 
     def handle(self, *args, **options):
+        f=FInstance.objects.filter(project_fxf__is_staged=True, site_fxf=None, site=None).values_list('instance', flat=True)
+        fm =list(settings.MONGO_DB.instances.find({ "_id": { "$in": f } }))
         import pdb; pdb.set_trace()
