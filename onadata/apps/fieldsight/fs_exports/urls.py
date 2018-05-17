1c28ce90cf1a57dc8bca9afe0735a283e9421a79
AASHISH - UBUNTU <aashish.baidya.c3@gmail.com>
2018-05-17 08:38:17 +0545

    migrate project and region done

--- onadata/apps/fieldsight/fs_exports/urls.py
+++ onadata/apps/fieldsight/fs_exports/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^xls/project/site/list/(?P<pk>[0-9]+)/$', views.ExportProjectSites.as_view(), name='export_xls_project_sites'),
     url(r'^xls/project/(?P<pk>[0-9]+)/region/(?P<region_id>[0-9]+)/site/list/$', views.ExportProjectSites.as_view(), name='export_xls_region_sites'),
+    url(r'^clone/project/from/(?P<pk>[0-9]+)/to/(?P<t_pk>[0-9]+)/$', views.CloneProjectSites.as_view(), name='clone_project_sites'),
+    url(r'^clone/project/from/(?P<pk>[0-9]+)/region/(?P<region_id>[0-9]+)/to/(?P<t_pk>[0-9]+)/$', views.CloneProjectSites.as_view(), name='clone_region_sites'),
     ]
--- onadata/apps/fieldsight/fs_exports/views.py
+++ onadata/apps/fieldsight/fs_exports/views.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 import xlwt
 from .. models import Project, Site
-from .. rolemixins import DonorRoleMixin
+from .. rolemixins import DonorRoleMixin, ProjectRoleMixin
 from django.views.generic import TemplateView, View
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
@@ -57,3 +57,60 @@ class ExportProjectSites(DonorRoleMixin, View):
             row_num += 1
         wb.save(response)
         return response
+
+
+
+class CloneProjectSites(ProjectRoleMixin, View):
+    def get(self, *args, **kwargs):
+        f_project=get_object_or_404(Project, pk=self.kwargs.get('pk'))
+        t_project=get_object_or_404(Project, pk=self.kwargs.get('t_pk'))
+        region_id = self.kwargs.get('region_id', None)
+        
+        #migrate metas
+        if not t_project.site_meta_attributes:
+            t_project.site_meta_attributes = f_project.site_meta_attributes
+        
+        region_map = {}
+
+        
+        def get_t_region_id(f_region_id):
+            # To get new region id without a query
+            if f_region_id in region_map:
+                return region_map[f_region_id]
+            else:
+                return None
+        
+        #migrate regions
+        if f_project.cluster_sites:
+            t_project.cluster_sites=True
+            
+            # To handle whole project or a single region migrate
+            if region_id:
+                regions = project.regions.filter(region_id=region_id)
+            else:
+                regions = project.regions.all()
+
+            for region in regions:
+                f_region_id = region.id
+                region.id=None
+                region.project_id=t_project.id
+                region.save()
+                t_region_id = region.id
+                region_map[f_region_id]=t_region_id
+        
+        t_project.save()
+
+        #migrate sites
+        if region_id:
+            sites = f_project.sites.filter(region_id=region_id)
+        else:
+            sites = f_project.sites.all()
+
+        if sites:
+            for site in sites:
+                site.id = None
+                site.project_id = t_project_id
+                site.region_id = get_t_region_id(site.region_id)
+                site.save()
+        return None
+
