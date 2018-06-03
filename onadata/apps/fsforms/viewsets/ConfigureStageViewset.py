2f306a874bddbc7fada46234e33bd56a7681bdc0
awemulya <awemulya@gmail.com>
2018-06-03 12:08:00 +0545

    paginate by 10

--- onadata/apps/fsforms/viewsets/ConfigureStageViewset.py
+++ onadata/apps/fsforms/viewsets/ConfigureStageViewset.py
@@ -125,7 +125,7 @@ class DeployViewset(viewsets.ModelViewSet):
     serializer_class = DeploySerializer
 
 class LargeResultsSetPagination(PageNumberPagination):
-    page_size = 100
+    page_size = 10
     # page_size_query_param = 'page_size'
     # max_page_size = 10000
 
