192de00fb9112283e349cc757cda71307d6011cb
awemulya <awemulya@gmail.com>
2018-07-29 14:34:48 +0545

    notification generator

---
+++ onadata/apps/fieldsight/management/commands/notifications.py
@@ -0,0 +1,40 @@
+import json
+from django.core.management.base import BaseCommand
+from channels import Group as ChannelGroup
+DAY_OF_THE_WEEK = {
+    0 : 'Monday',
+    1 : 'Tuesday',
+    2 : 'Wednesday',
+    3 : 'Thursday',
+    4 : 'Friday',
+    5 : 'Saturday',
+    6 : 'Sunday',
+}
+
+
+class Command(BaseCommand):
+    help = 'Create default groups'
+
+    def handle(self, *args, **options):
+        result = {
+            "id": 45639,
+            "source_uid": 147,
+            "source_name": "Kiran Shrestha",
+            "source_img": "/media/logo/default_user.png",
+            "get_source_url": "/users/profile/147/",
+            "get_event_name": "staged form Ring Beam",
+            "get_event_url": "/forms/forms/31523",
+            "get_extraobj_name": "TestHouse",
+            "get_extraobj_url": "/fieldsight/site-dashboard/4119/",
+            "get_absolute_url": "/events/notification/45639/",
+            "extra_json": '',
+            "type": 16,
+            "title": "new Site level Submission",
+            "date": "2018-07-17T11:13:41.440880Z",
+            "extra_message": '',
+            "recipient": '',
+            "seen_by": []
+        },
+        # ChannelGroup("notify-{}".format(1)).send({"text": json.dumps(result)})
+        ChannelGroup("notif-user-{}".format(1)).send({"text": json.dumps(result)})
+        self.stdout.write('Successfully created Notification')
