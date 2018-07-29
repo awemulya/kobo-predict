ffeb8c6d68c24bec6d83554285a2f2bf6a3deae9
awemulya <awemulya@gmail.com>
2018-07-29 15:12:17 +0545

    migrations merge

---
+++ onadata/apps/eventlog/migrations/0035_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eventlog', '0034_auto_20180725_1105'),
+        ('eventlog', '0033_auto_20180628_0524'),
+    ]
+
+    operations = [
+    ]
