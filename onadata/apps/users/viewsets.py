f5cd7bfebbbfa647fba8018e2aabac98b18ffc29
awemulya <awemulya@gmail.com>
2018-08-14 14:52:22 +0545

    MySitesResultsSetPagination 25"

--- onadata/apps/users/viewsets.py
+++ onadata/apps/users/viewsets.py
@@ -31,7 +31,7 @@ SAFE_METHODS = ('GET', 'POST')
 
 
 class MySitesResultsSetPagination(PageNumberPagination):
-    page_size = 100
+    page_size = 25
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
