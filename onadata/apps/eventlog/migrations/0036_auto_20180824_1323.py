1a891e6b4eb67003b1a2135e8f46d42ea5f24ab9
awemulya <awemulya@gmail.com>
2018-08-26 12:17:05 +0545

    multiple submissions

---
+++ onadata/apps/eventlog/migrations/0036_auto_20180824_1323.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eventlog', '0035_merge'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='celerytaskprogress',
+            name='task_type',
+            field=models.IntegerField(default=0, choices=[(0, 'Bulk Site Upload'), (1, 'Multi User Assign Project'), (2, 'Multi User Assign Site'), (3, 'Report Generation'), (4, 'Site Import'), (5, 'Xls export'), (6, 'Zip Site Images')]),
+        ),
+    ]
