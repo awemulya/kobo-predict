fc5dec02517c80ebd875b2ad31e31bfcc45c47a1
aasis <aashish.baidya.c3@gmail.com>
2018-08-26 21:21:19 +0545

    added refined regions list based on roles

--- onadata/apps/fieldsight/tasks.py
+++ onadata/apps/fieldsight/tasks.py
@@ -111,9 +111,9 @@ def UnassignUser(task_prog_obj_id, user_id, sites, regions, projects, group_id):
 
             if projects:
                 for project_id in projects: 
-                    regions = Region.objects.get(project_id = project_id)
+                    regions = Region.objects.get(project_id = project_id[1:])
                     for region in regions:
-                        sites = Site.objects.filter(region_id=region[1:])    
+                        sites = Site.objects.filter(region_id=region)    
                         count = count + sites.count()
                         for site_id in sites:
                             roles=UserRole.objects.filter(user_id=user, site_id = site_id, ended_at=None)
