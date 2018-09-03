6a85bd2ab64b11af6dfc3c099d1dbd913c5ba8cb
awemulya <awemulya@gmail.com>
2018-09-03 11:46:49 +0545

    submission project level forms

--- onadata/apps/fsforms/viewsets/FSXFormSubmissionApiViewset.py
+++ onadata/apps/fsforms/viewsets/FSXFormSubmissionApiViewset.py
@@ -98,115 +98,31 @@ class ProjectFSXFormSubmissionApi(XFormSubmissionApi):
             return self.error_response("Fieldsight Form ID Not Given", False, request)
         try:
             fs_proj_xf = get_object_or_404(FieldSightXF, pk=kwargs.get('pk'))
-            fxf = None
-            xform  = None
-            try:
-                if fs_proj_xf.is_survey:
-                    xform = fs_proj_xf.xf
-                elif fs_proj_xf.is_scheduled and siteid:
-                    site = Site.objects.get(pk=siteid)
-                    schedule = fs_proj_xf.schedule
-                    selected_days = tuple(schedule.selected_days.all())
-                    s, created = Schedule.objects.get_or_create(name=schedule.name, site=site, date_range_start=schedule.date_range_start,
-                                                date_range_end=schedule.date_range_end)
-                    if created:
-                        s.selected_days.add(*selected_days)
-                        s.save()
-                    fxf, created = FieldSightXF.objects.get_or_create(is_scheduled=True, site=site,
-                                                                      xf=fs_proj_xf.xf, from_project=True,
-                                                                      fsform=fs_proj_xf, schedule=s)
-                    xform = fxf.xf
-                elif (fs_proj_xf.is_scheduled is False and fs_proj_xf.is_staged is False) and siteid:
-                    site = Site.objects.get(pk=siteid)
-                    fxf, created = FieldSightXF.objects.get_or_create(is_scheduled=False,is_staged=False, site=site,
-                                                   xf=fs_proj_xf.xf, from_project=True, fsform=fs_proj_xf)
-                    xform = fxf.xf
-                elif fs_proj_xf.is_staged and siteid:
-                    site = Site.objects.get(pk=siteid)
-                    project_stage = fs_proj_xf.stage
-                    try:
-                        site_stage = Stage.objects.get(site=site, project_stage_id=project_stage.id)
-                        fxf = site_stage.stage_forms
-                        xform = fxf.xf
-                    except Exception as e:
-                        # return self.error_response("This Stage form not deployed in this site. Please Contact Administrators", False, request)
-                        try:
-                            with transaction.atomic():
-                                project = fs_proj_xf.project
-                                project_main_stages = project.stages.filter(stage__isnull=True)
-                                for pms in project_main_stages:
-                                    project_sub_stages = Stage.objects.filter(stage__id=pms.pk, stage_forms__is_deleted=False)
-                                    site_main_stage, created = Stage.objects.get_or_create(name=pms.name, order=pms.order,
-                                                                                           site=site,
-                                                                                           description=pms.description,
-                                                                                           project_stage_id=pms.id)
-                                    for pss in project_sub_stages:
-                                        if pss.tags and site.type:
-                                            if not site.type.id in pss.tags:
-                                                continue
-                                        site_sub_stage, created = Stage.objects.get_or_create(name=pss.name, order=pss.order, site=site,
-                                                       description=pss.description, stage=site_main_stage, project_stage_id=pss.id, weight=pss.weight)
-                                        if FieldSightXF.objects.filter(stage=pss).exists():
-                                            project_fsxf = pss.stage_forms
-                                            site_form, created = FieldSightXF.objects.get_or_create(is_staged=True, xf=project_fsxf.xf,
-                                                                                                    site=siteid,fsform=project_fsxf,
-                                                                                                    stage=site_sub_stage,
-                                                                                                    is_deployed=True)
-                                            if project_fsxf.id == fs_proj_xf.id:
-                                                fxf = site_form
-                                                xform = fxf.xf
-                        except Exception as e:
-                            return self.error_response("Error Occured in submission {0}".format(str(e)), False, request)
-                        if not (xform and fxf):
-                            return self.error_response("This Stage form not deployed in this site. Please Contact Administrators", False, request)
-            except Exception as e:
-                xform = fs_proj_xf.xf
+            xform  = fs_proj_xf.xf
             proj_id = fs_proj_xf.project.id
-
-        except:
-            return self.error_response("Site Id Or Form ID Not Vaild", False, request)
+            if siteid:
+                Site.objects.get(pk=siteid)
+        except Exception as e:
+            return self.error_response("Site Id Or Project Form ID Not Vaild", False, request)
 
         if request.method.upper() == 'HEAD':
             return Response(status=status.HTTP_204_NO_CONTENT,
                             headers=self.get_openrosa_headers(request),
                             template_name=self.template_name)
-        site_fsxf_id = None
-        if fxf:
-            site_fsxf_id = fxf.id
-        if fs_proj_xf.is_survey:
-            error, instance = create_instance_from_xml(request, None, None, fs_proj_xf.id, proj_id, xform)
-        else:
-            error, instance = create_instance_from_xml(request, site_fsxf_id, siteid, fs_proj_xf.id, proj_id, xform)
+        error, instance = create_instance_from_xml(request, None, siteid, fs_proj_xf.id, proj_id, xform)
 
-        
         if error or not instance:
             return self.error_response(error, False, request)
 
-        if fs_proj_xf.is_survey:
-            noti = instance.fieldsight_instance.logs.create(source=self.request.user, type=16, title="new Project level Submission",
-                                       organization=fs_proj_xf.project.organization,
-                                       project=fs_proj_xf.project,
-                                                        extra_object=fs_proj_xf.project,
-                                                        extra_message="project",
-                                                        content_object=instance.fieldsight_instance)
-        else:
-            site=Site.objects.get(pk=siteid)
-            noti = instance.fieldsight_instance.logs.create(source=self.request.user, type=16, title="new Site level Submission",
-                                       organization=fs_proj_xf.project.organization,
-                                       project=fs_proj_xf.project, site=site,
-                                                        extra_object=site,
-                                                        content_object=instance.fieldsight_instance)
-        result = {}
-        result['description'] = noti.description
-        result['url'] = noti.get_absolute_url()
-        # ChannelGroup("notify-{}".format(self.object.project.organization.id)).send({"text": json.dumps(result)})
-        # ChannelGroup("project-{}".format(self.object.project.id)).send({"text": json.dumps(result)})
-        if instance.fieldsight_instance.site:
-            ChannelGroup("site-{}".format(instance.fieldsight_instance.site.id)).send({"text": json.dumps(result)})
-        else:
-            ChannelGroup("project-{}".format(instance.fieldsight_instance.project.id)).send({"text": json.dumps(result)})
-
-        # modify create instance
+        instance.fieldsight_instance.logs.create(
+            source=self.request.user,
+            type=16,
+            title="new Project level Submission",
+            organization=fs_proj_xf.project.organization,
+            project=fs_proj_xf.project,
+            extra_object=fs_proj_xf.project,
+            extra_message="project",
+            content_object=instance.fieldsight_instance)
 
         
 
