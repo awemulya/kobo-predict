76ef2c4ac6f9bd58f9cfb62f999f76bb7091912d
awemulya <awemulya@gmail.com>
2018-09-05 11:52:51 +0545

    create stage in site fix

--- onadata/apps/fsforms/viewsets/FSXFormSubmissionApiViewset.py
+++ onadata/apps/fsforms/viewsets/FSXFormSubmissionApiViewset.py
@@ -103,63 +103,26 @@ class ProjectFSXFormSubmissionApi(XFormSubmissionApi):
             return self.error_response("siteid Invalid", False, request)
         if fsxfid is None:
             return self.error_response("Fieldsight Form ID Not Given", False, request)
-        site_or_project_form = get_object_or_404(FieldSightXF, pk=kwargs.get('pk'))
-        if not site_or_project_form.site and not site_or_project_form.project:
+        deleted_forms = {871066: 894803, 871067: 894804,871068: 894805, }
+        # First tranche deleted case
+        form_pk = kwargs.get('pk')
+        form_pk = int(form_pk)
+        fs_proj_xf = None
+        if deleted_forms.get(form_pk, False):
+            fs_proj_xf = get_object_or_404(FieldSightXF, pk=deleted_forms.get(form_pk))
+        site_or_project_form = None
+        if not fs_proj_xf and FieldSightXF.objects.filter(pk=form_pk).exists():
+            site_or_project_form = FieldSightXF.objects.get(pk=form_pk)
+        if site_or_project_form and not site_or_project_form.site and not site_or_project_form.project and not fs_proj_xf:
             return self.error_response("Orphan FieldsightXForm No Site or Form ID", False, request)
 
-        if site_or_project_form.site and not site_or_project_form.project:
-            fsxfid = kwargs.get('pk', None)
-            siteid = kwargs.get('site_id', None)
-            if fsxfid is None or siteid is None:
-                return self.error_response("Site Id Or Form ID Not Given", False, request)
-            try:
-                fsxfid = int(fsxfid)
-                fxf = get_object_or_404(FieldSightXF, pk=kwargs.get('pk'))
-                fs_proj_xf = fxf.fsform.pk if fxf.fsform else None
-                proj_id = fxf.site.project.pk
-                xform = fxf.xf
-                # site_id = fxf.site.pk if fxf.site else None
-            except:
-                return self.error_response("Site Id Or Form ID Not Vaild", False, request)
-
-            if request.method.upper() == 'HEAD':
-                return Response(status=status.HTTP_204_NO_CONTENT,
-                                headers=self.get_openrosa_headers(request),
-                                template_name=self.template_name)
-            error, instance = create_instance_from_xml(request, fsxfid, siteid, fs_proj_xf, proj_id, xform)
-            extra_message = ""
-
-            if fxf.is_survey:
-                extra_message = "project"
-
-            noti = instance.fieldsight_instance.logs.create(source=self.request.user, type=16, title="new Submission",
-                                                            organization=instance.fieldsight_instance.site.project.organization,
-                                                            project=instance.fieldsight_instance.site.project,
-                                                            site=instance.fieldsight_instance.site,
-                                                            extra_message=extra_message,
-                                                            extra_object=instance.fieldsight_instance.site,
-                                                            content_object=instance.fieldsight_instance)
-            result = {}
-            result['description'] = noti.description
-            result['url'] = noti.get_absolute_url()
-            # ChannelGroup("notify-{}".format(self.object.project.organization.id)).send({"text": json.dumps(result)})
-            # ChannelGroup("project-{}".format(self.object.project.id)).send({"text": json.dumps(result)})
-            ChannelGroup("site-{}".format(instance.fieldsight_instance.site.id)).send({"text": json.dumps(result)})
-
-            # modify create instance
-
-            if error or not instance:
-                return self.error_response(error, False, request)
-
-            context = self.get_serializer_context()
-            serializer = FieldSightSubmissionSerializer(instance, context=context)
-            return Response(serializer.data,
-                            headers=self.get_openrosa_headers(request),
-                            status=status.HTTP_201_CREATED,
-                            template_name=self.template_name)
+        if site_or_project_form and site_or_project_form.site and not site_or_project_form.project and not fs_proj_xf:
+            # Project level form submitted by site level form
+            fs_proj_xf = site_or_project_form.fsform
 
         try:
-            fs_proj_xf = get_object_or_404(FieldSightXF, pk=kwargs.get('pk'))
+            if not fs_proj_xf:
+                fs_proj_xf = get_object_or_404(FieldSightXF, pk=kwargs.get('pk'))
             fxf = None
             xform  = None
             try:
@@ -211,7 +174,7 @@ class ProjectFSXFormSubmissionApi(XFormSubmissionApi):
                                         if FieldSightXF.objects.filter(stage=pss).exists():
                                             project_fsxf = pss.stage_forms
                                             site_form, created = FieldSightXF.objects.get_or_create(is_staged=True, xf=project_fsxf.xf,
-                                                                                                    site=siteid,fsform=project_fsxf,
+                                                                                                    site=site,fsform=project_fsxf,
                                                                                                     stage=site_sub_stage,
                                                                                                     is_deployed=True)
                                             if project_fsxf.id == fs_proj_xf.id:
@@ -282,6 +245,4 @@ class ProjectFSXFormSubmissionApi(XFormSubmissionApi):
                         headers=self.get_openrosa_headers(request),
                         status=status.HTTP_201_CREATED,
                         template_name=self.template_name)
-
-
 
