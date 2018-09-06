7cb16a076739e4c9ff17093ee7cad02f2506b29d
awemulya <awemulya@gmail.com>
2018-09-06 11:52:07 +0545

    migration merge

---
+++ onadata/apps/fsforms/migrations/0060_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fsforms', '0059_projectgeojson'),
+        ('fsforms', '0059_auto_20180831_1029'),
+    ]
+
+    operations = [
+    ]
