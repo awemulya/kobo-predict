8de1eb051c4e80c0665db7f00835756cfd39fe8c
aasis <aashish.baidya.c3@gmail.com>
2018-09-11 14:31:35 +0545

    new migration added for eventlog types

--- onadata/apps/fieldsight/models.py
+++ onadata/apps/fieldsight/models.py
@@ -173,11 +173,11 @@ class Organization(models.Model):
     def get_organization_type(self):
         return self.type.name
 
-class ProjectManager(models.Manager):
+class ProjectManager(models.Manager, GeoManager):
     def get_queryset(self):
         return super(ProjectManager, self).get_queryset().filter(is_active=True)
 
-class ProjectAllManager(models.Manager):
+class ProjectAllManager(models.Manager, GeoManager):
     def get_queryset(self):
         return super(ProjectAllManager, self).get_queryset().all()
 
