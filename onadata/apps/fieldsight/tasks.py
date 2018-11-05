3e5d8c83592361ffcefc694d0535792d56ebaee3
aasis <aashish.baidya.c3@gmail.com>
2018-11-05 11:58:26 +0545

    extend soft time limit for progress table

--- onadata/apps/fieldsight/tasks.py
+++ onadata/apps/fieldsight/tasks.py
@@ -81,7 +81,7 @@ def site_download_zipfile(task_prog_obj_id, size):
                                        extra_message="@error " + u'{}'.format(e.message))
         buffer.close()                                                                      
 
-@shared_task(soft_time_limit=3200)
+@shared_task(soft_time_limit=4200)
 def generate_stage_status_report(task_prog_obj_id, project_id):
     task = CeleryTaskProgress.objects.get(pk=task_prog_obj_id)
     project = Project.objects.get(pk=project_id)
