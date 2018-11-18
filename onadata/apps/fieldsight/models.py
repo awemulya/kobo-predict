5a60f714940b52e0abeb08cc43b309413c562125
aasis <aashish.baidya.c3@gmail.com>
2018-11-18 16:06:02 +0545

    user profile url issue fixed

--- onadata/apps/fieldsight/models.py
+++ onadata/apps/fieldsight/models.py
@@ -19,7 +19,7 @@ from django.contrib.auth.models import Group
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.core.serializers import serialize
-from onadata.apps.fsforms.models import Stage
+
 from django.db.models import Q
 
 class TimeZone(models.Model):
