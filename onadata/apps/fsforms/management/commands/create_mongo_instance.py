bfb048fb4d35815de7f90ed4a8db1dc59d0250fd
awemulya <awemulya@gmail.com>
2018-12-05 13:18:23 +0545

    exclude old instances

--- onadata/apps/fsforms/management/commands/create_mongo_instance.py
+++ onadata/apps/fsforms/management/commands/create_mongo_instance.py
@@ -2,12 +2,17 @@ from django.core.management.base import BaseCommand
 from onadata.apps.logger.models import Instance
 from onadata.apps.viewer.models.parsed_instance import update_mongo_instance
 
+OLD_INSTANCES = [107, 10, 11, 16, 64, 73, 81, 85, 109, 112,
+                 99045, 6, 7, 34, 36, 39, 43, 47, 52, 55, 57, 62, 82, 87,
+                 99, 100, 113, 115, 118, 2188, 2, 9, 21, 37, 42, 63, 66,
+                 67, 72, 104, 105, 106, 117, 32788, 53712, 53713,
+                 53714, 53715, 57105]
 
 class Command(BaseCommand):
     help = 'Create mongo missing data'
 
     def handle(self, *args, **options):
-        instances = Instance.objects.filter(is_synced_with_mongo=False)
+        instances = Instance.objects.filter(is_synced_with_mongo=False).exclude(pk__in=OLD_INSTANCES)
         for i in instances:
             d = i.parsed_instance.to_dict_for_mongo()
             try:
