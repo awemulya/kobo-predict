92f66281a71e518e4b4ef0a6ff273fef6dbe5886
aasis <aashish.baidya.c3@gmail.com>
2018-12-30 17:29:44 +0545

    fixed

--- onadata/apps/fieldsight/views.py
+++ onadata/apps/fieldsight/views.py
@@ -231,7 +231,7 @@ class Project_dashboard(ProjectRoleMixin, TemplateView):
         active_supervisors = finstances.distinct('submitted_by').count()
 
         try:
-            site_visits = settings.MONGO_DB.instances.aggregate([{"$match":{"fs_project": obj.id}, { '$gte' : one_week_ago.isoformat() } },  { "$group" : { 
+            site_visits = settings.MONGO_DB.instances.aggregate([{"$match":{"fs_project": obj.id}, "start": { '$gte' : one_week_ago.isoformat() } },  { "$group" : { 
                   "_id" :  {        
                     "fs_site": "$fs_site",
                     "date": { "$substr": [ "$start", 0, 10 ] }
@@ -2954,7 +2954,7 @@ class DonorProjectDashboard(DonorRoleMixin, TemplateView):
         # site_visits = finstances.filter(site_id__isnull=False).distinct('site_id').count()
         active_supervisors = finstances.distinct('submitted_by').count()
         try:
-            site_visits = settings.MONGO_DB.instances.aggregate([{"$match":{"fs_project": obj.id}, { '$gte' : one_week_ago.isoformat()}},  { "$group" : { 
+            site_visits = settings.MONGO_DB.instances.aggregate([{"$match":{"fs_project": obj.id}, "start":{ '$gte' : one_week_ago.isoformat()}},  { "$group" : { 
                   "_id" :  {        
                     "fs_site": "$fs_site",
                     "date": { "$substr": [ "$start", 0, 10 ] }
