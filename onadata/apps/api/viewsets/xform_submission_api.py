17d9438abc980ac37b942cf41160d02678b83850
awemulya <awemulya@gmail.com>
2019-01-02 13:31:13 +0545

    clean

--- onadata/apps/api/viewsets/xform_submission_api.py
+++ onadata/apps/api/viewsets/xform_submission_api.py
@@ -180,22 +180,22 @@ Here is some example JSON, it would replace `[the JSON]` above:
 
     def create(self, request, *args, **kwargs):
         username = self.kwargs.get('username')
-        if self.request.user.is_anonymous():
-            if username is None:
-                # raises a permission denied exception, forces authentication
-                self.permission_denied(self.request)
-            else:
-                user = get_object_or_404(User, username=username.lower())
-
-                profile, created = UserProfile.objects.get_or_create(user=user)
-
-                if profile.require_auth:
-                    # raises a permission denied exception,
-                    # forces authentication
-                    self.permission_denied(self.request)
-        elif not username:
-            # get the username from the user if not set
-            username = (request.user and request.user.username)
+        # if self.request.user.is_anonymous():
+        #     if username is None:
+        #         # raises a permission denied exception, forces authentication
+        #         self.permission_denied(self.request)
+        #     else:
+        #         user = get_object_or_404(User, username=username.lower())
+        #
+        #         profile, created = UserProfile.objects.get_or_create(user=user)
+        #
+        #         if profile.require_auth:
+        #             # raises a permission denied exception,
+        #             # forces authentication
+        #             self.permission_denied(self.request)
+        # elif not username:
+        #     # get the username from the user if not set
+        #     username = (request.user and request.user.username)
 
         if request.method.upper() == 'HEAD':
             return Response(status=status.HTTP_204_NO_CONTENT,
