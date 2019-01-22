68b58dce171c81be1c338ce6477289090029af2e
raeesaroj <raesaroj16@gmail.com>
2019-01-22 14:15:10 +0545

    project submission form transfer update

--- onadata/apps/fsforms/management/commands/project_submission_form_transfer.py
+++ onadata/apps/fsforms/management/commands/project_submission_form_transfer.py
@@ -11,27 +11,45 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('submission_id', type=int)
         parser.add_argument('transfer_to', type=int)
+        parser.add_argument('only_mongo', type=str)
 
     def handle(self, *args, **options):
-        submission_id = options['submission_id']
-        transfer_to = options['transfer_to']
+        only_mongo = options.get("only_mongo", "0")
 
-        instance = Instance.objects.get(id=submission_id)
-        transfer_to = FieldSightXF.objects.get(id=transfer_to)
-        valid_xf = transfer_to.xf
-        instance.xform = valid_xf
-        instance.save()
+        if only_mongo == "0":
+            submission_id = options['submission_id']
+            transfer_to = options['transfer_to']
 
-        d = instance.parsed_instance.to_dict_for_mongo()
-        try:
-            x = instance.fieldsight_instance
-            d.update({'fs_project_uuid': str(x.project_fxf_id), 'fs_project': x.project_id, 'fs_status': 0,
-                      'fs_site': x.site_id, 'fs_uuid': x.site_fxf_id})
+            instance = Instance.objects.get(id=submission_id)
+            transfer_to = FieldSightXF.objects.get(id=transfer_to)
+            valid_xf = transfer_to.xf
+            instance.xform = valid_xf
+            instance.save()
+            fieldsight_instance = instance.fieldsight_instance
+            fieldsight_instance.project_fxf = transfer_to
+            fieldsight_instance.save()
+
+            # d = instance.parsed_instance.to_dict_for_mongo()
+        else:
             try:
-                synced = update_mongo_instance(d, instance.id)
-                print(synced, "updated in mongo success")
+                submission_id = options['submission_id']
+                transfer_to = options['transfer_to']
+
+                instance = Instance.objects.get(id=submission_id)
+                transfer_to = FieldSightXF.objects.get(id=transfer_to)
+                # valid_xf = transfer_to.xf
+                # instance.xform = valid_xf
+                # instance.save()
+
+                d = instance.parsed_instance.to_dict_for_mongo()
+                x = instance.fieldsight_instance
+                d.update({'fs_project_uuid': str(x.project_fxf_id), 'fs_project': x.project_id, 'fs_status': 0,
+                          'fs_site': x.site_id, 'fs_uuid': x.site_fxf_id})
+                try:
+                    synced = update_mongo_instance(d, instance.id)
+                    print(synced, "updated in mongo success")
+                except Exception as e:
+                    print(str(e))
             except Exception as e:
                 print(str(e))
-        except Exception as e:
-            print(str(e))
         self.stdout.write('Successfully transfer project submission form in "%s"' % transfer_to)
