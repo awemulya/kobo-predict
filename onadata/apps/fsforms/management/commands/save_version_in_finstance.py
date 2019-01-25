8ffc8cabd5a2ba20997337c101018f3c25faef4e
SanipRijal <saneprijal@gmail.com>
2019-01-25 10:50:20 +0545

    Update command to save version in finstance

--- onadata/apps/fsforms/management/commands/save_version_in_finstance.py
+++ onadata/apps/fsforms/management/commands/save_version_in_finstance.py
@@ -1,5 +1,8 @@
 from django.core.management.base import BaseCommand
 from onadata.apps.fsforms.models import FInstance
+from onadata.apps.logger.models import Instance
+from onadata.settings.local_settings import XML_VERSION_MAX_ITER
+import re
 
 
 class Command(BaseCommand):
@@ -15,6 +18,7 @@ class Command(BaseCommand):
         stop = False
         offset = 0
         while stop is not True:
+            n = XML_VERSION_MAX_ITER
             limit = offset + batchsize
             instances = FInstance.objects.filter(instance__xform__user__username=username, version='')[offset:limit]
             inst = list(instances)
@@ -24,8 +28,38 @@ class Command(BaseCommand):
                         inst[-1].id))
                 
                 for instance in instances:
-                    instance.save()
+                    i = Instance.objects.get(fieldsight_instance=instance)
+                    xml = i.xml
+                    
+                    pattern = re.compile('version="(.*)">')
+                    m = pattern.search(xml)
+                    if m:
+                        instance.version = m.group(1)
+                        instance.save()
+                        continue
+                    
+                    for i in range(n, 0, -1):
+                        p = re.compile('<_version__00{0}>(.*)</_version__00{1}>'.format(i, i))
+                        m = p.search(instance)
+                        if m:
+                            instance.version = m.group(1)
+                            instance.save()
+                            continue
 
+                    p = re.compile('<_version_>(.*)</_version_>')
+                    m = p.search(xml)
+                    if m:
+                        instance.version = m.group(1)
+                        instance.save()
+                        continue
+                    
+                    p1 = re.compile('<__version__>(.*)</__version__>')
+                    m1 = p1.search(xml)
+                    if m1:
+                        instance.version = m.group(1)
+                        instance.save()
+                        continue                    
+                    
             else:
                 stop = True
                 
