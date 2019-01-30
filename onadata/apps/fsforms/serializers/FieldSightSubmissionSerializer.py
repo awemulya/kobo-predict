b9d410de6bfd1c3197743c486a2d14c727e90200
awemulya <awemulya@gmail.com>
2019-01-30 15:41:31 +0545

    clean up

--- onadata/apps/fsforms/serializers/FieldSightSubmissionSerializer.py
+++ onadata/apps/fsforms/serializers/FieldSightSubmissionSerializer.py
@@ -1,16 +1,17 @@
 from django.utils.translation import ugettext as _
 from onadata.libs.serializers.data_serializer import SubmissionSerializer
 
+
 class FieldSightSubmissionSerializer(SubmissionSerializer):
     def to_representation(self, obj):
-        if not hasattr(obj, 'FieldSightXF'):
+        if not hasattr(obj, 'xform'):
             return super(FieldSightSubmissionSerializer, self).to_representation(obj)
 
         return {
             'message': _("Successful submission."),
-            'formid': obj.xf.xform.id_string,
-            'encrypted': obj.xf.xform.encrypted,
-            'instanceID': u'uuid:%s' % obj.id,
+            'formid': obj.xform.id_string,
+            'encrypted': obj.xform.encrypted,
+            'instanceID': u'uuid:%s' % obj.uuid,
             'finstanceID': u'%s' % obj.fieldsight_instance.id,
             'submissionDate': obj.date_created.isoformat(),
             'markedAsCompleteDate': obj.date_modified.isoformat()
