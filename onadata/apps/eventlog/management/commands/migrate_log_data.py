9e70b554e1d4cffea5dd2de218bfdf78f6e63c1f
aasis <aashish.baidya.c3@gmail.com>
2019-02-17 16:31:17 +0545

    added management command for log migrate

---
+++ onadata/apps/eventlog/management/commands/migrate_log_data.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand
+from onadata.apps.eventlog.models import *
+
+class Command(BaseCommand):
+    help = 'Migrate log data.'
+
+    def handle(self, *args, **options):
+        _input = raw_input('Enter comma-delimited list of ids: ').split(',')
+
+        if len(_input) > 2:
+            return
+
+        start = _input[0]
+        end = _input[1]
+        logs = FieldSightLogs.objects.all()[start:end]
+
+        for log in logs:
+            log.save()
+
+        self.stdout.write('Successfully completed.')
+
+
+    
+    extra_obj_name = models.CharField(max_length=255, blank=True)
+    event_name = models.CharField(max_length=255, blank=True)
+
+
+
+    def save(self, *args, **kwargs):
+        try:
+            self.event_name = self.content_object.getname()
+        except:
+            self.event_name = ""
+        try:
+            if self.extra_object is None:
+                self.extra_obj_name = ""
+
+            elif self.extra_content_type.model == "user":
+                if self.extra_object.user_profile:
+                    self.extra_obj_name =  self.extra_object.user_profile.getname()
+                else:
+                    self.extra_obj_name =  self.extra_object.email
+            elif:
+                self.extra_obj_name =  self.extra_object.getname()
+        except:
+            self.extra_obj_name = ""
+        
+        super(FieldSightLog, self).save(*args, **kwargs)
+        
