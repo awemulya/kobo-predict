bf4b740d2eed175ece3eb06d927c4fed6b51ceb5
aasis <aashish.baidya.c3@gmail.com>
2019-02-24 16:06:57 +0545

    report for site export added and minor static url in js converted to dynamic url

--- onadata/apps/fieldsight/urls.py
+++ onadata/apps/fieldsight/urls.py
@@ -135,7 +135,7 @@ urlpatterns = [
     url(r'^api/sites/$', AllSiteViewSet.as_view({'get': 'list'}), name='sites-list'),
     url(r'^api/project-types/$', ProjectTypeViewset.as_view({'get': 'list'})),
 
-    url(r'^api/site-types/(?P<pk>\d+)/$', SiteTypeViewset.as_view({'get': 'list'})),
+    url(r'^api/site-types/(?P<pk>\d+)/$', SiteTypeViewset.as_view({'get': 'list'}), name='project_site_types_api'),
     url(r'^api/site-types/$', SiteTypeViewset.as_view({'get': 'list', 'post':'create'})),
 
     url(r'^api/survey-sites/(?P<pk>\d+)/$', SiteCreationSurveyViewSet.as_view({'get': 'list'}), name='sites-list'),
