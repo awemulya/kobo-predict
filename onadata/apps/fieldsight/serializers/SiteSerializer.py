b06039701e573cde3045c77cc84e3a0f2fe5a1d5
awemulya <awemulya@gmail.com>
2019-03-10 15:06:25 +0545

    site type bug fixed

--- onadata/apps/fieldsight/serializers/SiteSerializer.py
+++ onadata/apps/fieldsight/serializers/SiteSerializer.py
@@ -113,21 +113,32 @@ class SiteCreationSurveySerializer(serializers.ModelSerializer):
     class Meta:
         model = Site
         read_only_fields = ('logo', 'location')
-        exclude = ('current_progress', 'current_status')
+        exclude = ('current_progress', 'current_status', 'type', 'region')
 
     def create(self, validated_data):
         p = Point(float(validated_data.pop('longitude')), float(validated_data.pop('latitude')),srid=4326)
-        validated_data.update({'is_survey': False,'is_active':True,'location':p,})
-        region = validated_data.get('region', False)
-        site_type = validated_data.get('type', False)
-        if region and isinstance(region, int):
-            if not Region.objects.filter(pk=region, is_active=True).exists():
-                validated_data.pop('region')
+        validated_data.update({'is_survey': False,'is_active':True,'location':p})
 
-        if site_type and isinstance(site_type, int):
-            if not SiteType.objects.filter(pk=site_type, deleted=False).exists():
-                validated_data.pop('site_type')
         site = Site.objects.create(**validated_data)
+
+        region = self.context['request'].data.get('region', False)
+        site_type = self.context['request'].data.get('type', False)
+        try:
+            site_type = int(site_type)
+        except Exception as e:
+            site_type = False
+        if site_type and isinstance(site_type, int):
+            if SiteType.objects.filter(pk=site_type, deleted=False).exists():
+                site.type = SiteType.objects.get(pk=site_type)
+                site.save()
+        try:
+            region = int(region)
+        except Exception as e:
+            region = False
+        if region and isinstance(region, int):
+            if Region.objects.filter(pk=region, is_active=True).exists():
+                site.region = Region.objects.get(pk=region)
+
         image = self.context['request'].FILES.values()
         for img in image:
             SiteCreateSurveyImages.objects.create(image=img, site=site)
